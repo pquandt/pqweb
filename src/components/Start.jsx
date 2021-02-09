5c30cdea6463ba7331788ef0c1092a535b26862c
pquandt <pquandt84@gmail.com>
2021-02-09 19:05:25 +0100

    add react

---
+++ src/components/Start.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Start.css";
+
+function Start() {
+  return (
+    <body>
+      <div>
+        <h1 class="name">Patrick Quandt</h1>
+      </div>
+    </body>
+  );
+}
+
+export default Start;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Start from './components/Start';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Start />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
