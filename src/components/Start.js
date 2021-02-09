5836174372a0f2f0a672bb2e5c5e8ffdfe76732b
pquandt <pquandt84@gmail.com>
2021-02-09 20:13:21 +0100

    add greeting but bugged

---
+++ src/components/Start.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Start.css";
+// import TimeOfDay from "./TimeOfDay";
+
+function Start() {
+  return (
+    <div>
+      <div>
+        <h1 className="name"> Patrick Quandt</h1>
+      </div>
+      <div>
+        <p>Guten </p>
+      </div>
+    </div>
+  );
+}
+
+export default Start;
