988ee1354f0c48b1ea2a53133731824c608cbe71
pquandt <pquandt84@gmail.com>
2021-07-12 15:28:45 +0200

    cleaned css and added contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -15,7 +15,25 @@ export default function Contact() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <div className="contact-button-mail">Email</div>
+              <div className="contact-button">Email</div>
+            </a>
+          </div>
+          <div>
+            <a
+              href="https://github.com/pquandt"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div className="contact-button">Github</div>
+            </a>
+          </div>
+          <div>
+            <a
+              href="https://www.linkedin.com/in/patrick-quandt-8848841b3/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div className="contact-button">LinkedIn</div>
             </a>
           </div>
         </div>
